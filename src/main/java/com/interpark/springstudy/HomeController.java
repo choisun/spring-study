7bcc0ad6829a62008c5f23c05091060f67131111
최선 <sun228@interpark.com>
2017-07-31 17:09:22 +0900

    path variable

--- src/main/java/com/interpark/springstudy/HomeController.java
+++ src/main/java/com/interpark/springstudy/HomeController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -48,6 +49,13 @@ public class HomeController {
 		
 		model.addAttribute("name", dto.getName());
 		model.addAttribute("age", dto.getAge());
+		return "home2";
+	}
+	
+	@RequestMapping(value = "/data/{name}", method = RequestMethod.GET)
+	public String pathValue(@PathVariable String name, Model model) {
+		
+		model.addAttribute("name", name);
 		return "home2";
 	}
 }
