26b9fe278de1a4fa56ca92847f69ff5228b1f397
choisun <choisun0228@gmail.com>
2017-07-31 22:35:38 +0900

    mysql 연동 수정
    Signed-off-by: choisun <choisun0228@gmail.com>

--- src/main/java/com/interpark/springstudy/controller/HomeController.java
+++ src/main/java/com/interpark/springstudy/controller/HomeController.java
@@ -14,9 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.interpark.springstudy.dao.HomeDao;
 import com.interpark.springstudy.dto.TestDto;
 import com.interpark.springstudy.service.HomeService;
 
